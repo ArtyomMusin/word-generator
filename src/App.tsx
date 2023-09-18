6110b4993152094e2a60fa7cc9036663880d9742
Artem <atm759@mail.ru>
2023-09-18 23:32:54 +0200

    headings has been created

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Header from './components/total/Header'
+import Routes from './Routes'
+
+function App() {
+  return (
+      <div className="App p-3">
+        <Header />
+        <Routes />
+      </div>
+  )
+}
+
+export default App;
---
+++ src/components/pages/my_try/wordGenerator/config.ts
@@ -0,0 +1,28 @@
+export const config = {
+    font: 'Arial',
+    heading1: {
+        color: '#575AEF',
+        size: 28,
+        font: 'Arial',
+        bold: true
+    },
+    heading2: {
+        color: '#575AEF',
+        size: 24,
+        font: 'Arial',
+        bold: true
+    },
+    heading3: {
+        color: '#000000',
+        size: 22,
+        font: 'Arial',
+        bold: true
+    },
+    heading4: {
+        color: '#000000',
+        size: 20,
+        font: 'Arial',
+        bold: true,
+        italics: false
+    }
+}
